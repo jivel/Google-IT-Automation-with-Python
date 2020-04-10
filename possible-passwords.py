e67bd1ee8b110a866a59314cd56faaea6a0aeba1
jimenezlav <jivel91@gmail.com>
2020-04-10 07:17:44 -0500

    refactor: use cartesian product

--- possible-passwords.py
+++ possible-passwords.py
@@ -2,13 +2,14 @@
 # For a 1 letter password, there would be 26 possibilities. 
 # For a 2 letter password, each letter is independent of the other, so there would be 26 times 26 possibilities. 
 # Using this information, print the amount of possible passwords that can be formed with 6 letters.
-from itertools import combinations_with_replacement 
+from itertools import product
 
 alphabet = map(chr, range(97, 123))
 
 # Get all combinations of ["a", "b", "c",...,"z"] and length X letters 
-passwordLength = 6
-differentPasswords  = list(combinations_with_replacement(alphabet, passwordLength))
+passwordLength = 5
+
+differentPasswords  = list(product(alphabet, repeat=passwordLength))
 total = len(differentPasswords)
 print(total)
 # Print the obtained combinations 
