13588e6ca541af35badce6b91805f090efc86840
Luis Antonio Jiménez Velasco <jivel_91@hotmail.com>
2020-06-15 16:01:01 -0500

    feat: Telnet client whith telnetlib
    write and read in socket

---
+++ telnet-client.py
@@ -0,0 +1,26 @@
+import telnetlib
+
+HOST_NX1 = "localhost"
+PORT_NX1 = 10000
+ENCODING = 'ISO-8859-1'
+
+def open_socket(host, port):
+	return telnetlib.Telnet(host, port)
+
+def get_input(file) -> str:
+    file_input = open(file, "r", encoding=ENCODING)
+    return file_input.read()
+
+def save_response(file, response):
+	file_response = open(file,"w+", encoding=ENCODING)
+	file_response.write(response)
+	file_response.close()
+
+def send():
+	with open_socket(HOST_NX1, PORT_NX1) as tn:
+		input = get_input("input.txt") 
+		tn.write(b"%s\n" % input.encode(ENCODING))
+		save_response("response.txt", tn.read_all().decode(ENCODING))
+
+if __name__== "__main__":
+    send()
