0d6e3404a7b71272559b04f4fa77e5e71db5e399
Luis Antonio Jiménez Velasco <jivel_91@hotmail.com>
2020-06-22 21:12:41 -0500

    create search-recursively-in-gz-files.py
    count match and show the file gz

---
+++ search-recursively-in-gz-files.py
@@ -0,0 +1,36 @@
+import pathlib
+import gzip
+import re
+import argparse
+
+fileDir = r"/home/"
+fileExt = r"**\*.gz"
+ENCODING = 'ISO-8859-1'
+
+def get_paths_gz():
+    return [str(gz_path) for gz_path in pathlib.Path(fileDir).glob(fileExt)]
+
+
+def search_recursively_in_gz_files(text):
+    paths_gz = get_paths_gz()
+    total = 0
+    for path_gz in paths_gz:
+        total += find_text_in_gz_file(path_gz, text)
+    print("Total: {}".format(total))
+
+def find_text_in_gz_file(path_gz, text):
+    with gzip.open(path_gz, 'rb') as input_file:
+        all_data = input_file.read().decode(ENCODING)
+        lines = re.findall(r'{}'.format(text), all_data)
+        if lines:
+            print('Total: {} Path file: {}'.format(len(lines), path_gz))
+            return len(lines)
+        return 0
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--text", "-t")
+args = parser.parse_args()
+
+if args.text:
+    print("searching {} in {}".format(args.text, fileDir))
+    search_recursively_in_gz_files(args.text)
